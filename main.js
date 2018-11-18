0c34cc421086aa232f0b4a0bfd0ebed4e30884bf
Alex Machado <amachado@sva.edu>
2018-11-18 15:33:29 -0500

    fix mobile scaling issue

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 // will find window width
 
+console.log(document.documentElement.clientWidth)
+
+var width = document.documentElement.clientWidth
+var menu = document.getElementsByClassName('menu')
+const archive = menu[0].children[2]
+
+console.log(archive)
+console.log(menu[0].children[2]);
+
+
+
+function displayChange(){
+    if (width < 768) {
+        archive.classList.add('not-visible')
+    }
+    else {
+        archive.classList.remove('visible')
+    }
+}
